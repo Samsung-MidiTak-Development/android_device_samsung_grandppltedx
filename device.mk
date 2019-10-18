f083efdc0278a783cb28a8bc5f795e58316fde8e
Sundong Ahn <sundongahn@google.com>
2019-10-18 17:51:23 +0100

    grandppltedx: Add system properties for surfaceflinger
    The configstore will be not used in surfaceflinger. So surfaceflinger
    will use the system properties instead of configstore.
    For this, the system properties are added.
    
    Bug: 112386364
    Test: m -j && boot test
    Change-Id: Ie3d2adccca06702217984cf2c9db986d852b7014

--- device.mk
+++ device.mk
@@ -82,6 +82,9 @@ PRODUCT_PACKAGES += \
 	android.hardware.graphics.mapper@2.0-impl \
 	android.hardware.graphics.composer@2.1-impl
 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
+
 # GPS
 PRODUCT_PACKAGES += \
 	wifi2agps \
