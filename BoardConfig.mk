2b451443d86e99f734b8683428e8c2159b7d5865
Almond <iwanttobebetter7@gmail.com>
2022-02-02 16:21:38 +0700

    shim: simplify shimming and fix agpsd shim
    Signed-off-by: Almond

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -167,6 +167,7 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.mt6735
 # Move symlinks here
 TARGET_LD_SHIM_LIBS := \
 	/vendor/bin/mtk_agpsd|libshim_gps.so \
+	/vendor/bin/mtk_agpsd|libshim_agpsd.so \
 	/vendor/lib/libcam.utils.sensorlistener.so|libandroid.so \
 	/vendor/lib/libcam_utils.so|libshim_nv21e_camera.so \
 	/vendor/lib/libcam_utils.so|libshim_camera.so \
