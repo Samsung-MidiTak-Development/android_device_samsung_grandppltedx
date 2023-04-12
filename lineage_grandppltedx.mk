c02f8f25b17b3e816e07097ae4d0c24f2f364a73
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-04-12 22:07:35 +0700

    grandppltedx: Drop core.mk
    Fixes
    device/samsung/grandppltedx/lineage_grandppltedx.mk:31: error: _nic.PRODUCTS.[[device/samsung/grandppltedx/lineage_grandppltedx.mk]]: "build/make/target/product/core.mk" does not exist.
    
    Signed-off-by: Nguyễn Long <hoanglong.lumi404@gmail.com>

--- lineage_grandppltedx.mk
+++ lineage_grandppltedx.mk
@@ -5,7 +5,6 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
