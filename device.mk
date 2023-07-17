d4ee840b80a81b13d942c254378eac5df7eeed1f
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-07-17 22:17:21 +0700

    grandppltedx: Binderize Keymaster HAL
    Signed-off-by: Nguyễn Long <hoanglong.lumi404@gmail.com>

--- device.mk
+++ device.mk
@@ -264,7 +264,9 @@ PRODUCT_PACKAGES += \
 	android.hardware.light@2.0-impl-mediatek
 
 # Keymaster
-PRODUCT_PACKAGES += android.hardware.keymaster@3.0-impl
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Memtrack
 PRODUCT_PACKAGES += android.hardware.memtrack@1.0-impl
